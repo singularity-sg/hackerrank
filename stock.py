748dc01661d58dc9a6148cc99c04b5f95b6a7e88
SG Development Team <sgextreme@wds.co>
2013-02-21 23:54:10 +0800

    Made changes

--- stock.py
+++ stock.py
@@ -9,6 +9,9 @@ stocks = {}
 # A dictionary with k=stock name and v=oscillation
 stocks_oscillation = {}
 
+# A dictionary with k=stock name and v=difference with median price
+stocks_median = {}
+
 # Stocks to purchase
 stocks_purchased = [] 
 
@@ -22,8 +25,9 @@ def readData():
             for line in stocks_file:
                 stock_prices = [] 
                 temp = line.split() 
-                stock_prices.append([float(i) for i in temp[1:]])
+                stock_prices = [float(i) for i in temp[1:]]
                 stocks[temp[0]] = stock_prices
+        #print "Stocks after reading : {0}".format(stocks)
     except EnvironmentError:
         f = open('stock.txt','w+')
 
@@ -42,63 +46,102 @@ def writeData():
 
 def calculate_oscillation(name, prices):
     global stocks_oscillation
-    oscillation = 0.0
-
-    for i in xrange(0, len(prices)):
-        if i == 1:
-            continue
-        change = prices[i] - prices[i-1]
-        tmp = change / prices[i-1] 
-    oscillation = oscillation + tmp
+    window = 5 
+    acceleration = 0.0
+    price_len = len(prices)
+   
+    if len(prices) < window:
+        stocks_oscillation[name] = acceleration
+        return
     
-    stocks_oscillation[name] = oscillation
+    smoothed_prices = []
+    no_of_windows = int(math.floor(price_len / window))
 
-def analyse(money, name, owned):
+    for i in xrange(0, price_len):
+        window_sum = 0.0
+        window_avg = 0.0
+        if (i+window-1) > price_len:
+            for j in xrange(i,price_len):
+                window_sum = window_sum + prices[j]
+            window_avg = window_sum / (price_len - i)
+        else:
+            for j in xrange(i, i+window-1):
+                window_sum = window_sum + prices[j]
+            window_avg = window_sum / window
+            
+        smoothed_prices.append(window_avg)
+    
+    print "Smoothed Prices: {0}".format(smoothed_prices)
+
+    if len(smoothed_prices) > 1:
+        change = smoothed_prices[-1] - smoothed_prices[-2]
+        if smoothed_prices[-2] > 0:
+            acceleration = change / smoothed_prices[-2]
+            
+    stocks_oscillation[name] = acceleration
+
+def calculate_median(name, prices):
+    global stocks_median
+    sorted_prices = sorted(prices)
+    median = int(math.floor(len(sorted_prices) / 2))
+    median_price = sorted_prices[median]
+    change = prices[-1] - median_price
+    stocks_median[name] = (change / median_price)
+
+def analyse(money, name, owned, days_left):
     global stocks
     global stocks_oscillation
 
     for n in name:
         price = stocks[n]
         calculate_oscillation(n, price)
-
-    sell_stocks(name, owned)
-    select_stocks(money)
+        calculate_median(n, price)
     
-def sell_stocks(name, owned):
+    #print "Stock oscillation : {0}".format(stocks_oscillation)
+    #print "Stock median : {0}".format(stocks_median)
+    sell_stocks(name, owned, (days_left==1))
+    select_stocks(money, days_left)
+    
+def sell_stocks(name, owned, last_day):
     global stocks_sold
-    
+   
     for i in xrange(0, len(name)):
         n = name[i]
         o = owned[i]
         osc = stocks_oscillation[n]
-    if osc < 0 and o > 0:
-        stocks_sold.append((n,o))
+        median = stocks_median[n]
 
-def select_stocks(money):
+        if (last_day):
+            if o > 0:
+                stocks_sold.append((n,o))
+        elif osc < 0.2  and o > 0 and median > 0:
+            stocks_sold.append((n,o))
+
+def select_stocks(money, days_left):
     global stocks
     global stocks_purchased
     global stocks_oscillation
+    global stocks_median
 
-    proportion = {1:[1.0], 2:[0.7,0.3], 3:[0.5,0.3,0.2], 4:[0.4,0.3,0.2,0.1]}
-    sorted_stocks_oscillation = sorted(stocks_oscillation.iteritems(), key = itemgetter(1), reverse=True)
+    #sorted_stocks_oscillation = sorted(stocks_oscillation.iteritems(), key = itemgetter(1), reverse=True)
+    sorted_stocks_median = sorted(stocks_median.iteritems(), key = itemgetter(1), reverse=False)
    
-    #print "Stock oscillation : {0}".format(sorted_stocks_oscillation)
-
     purchase_stock = []
 
-    for i in xrange(0, len(sorted_stocks_oscillation)):
-        if i > 3:
-            break
-        name = sorted_stocks_oscillation[i][0]
-        osc = sorted_stocks_oscillation[i][1]
+    for i in xrange(0, len(sorted_stocks_median)):
+        name = sorted_stocks_median[i][0]
+        median = stocks_median[name]
+        osc = stocks_oscillation[name]
         price = stocks[name][-1]
-        if osc > 0:
+        if osc > 0.2 and median < -0.4 and days_left > 10:
             purchase_stock.append((name,price))
    
     #print "Purchase Stock : {0}".format(purchase_stock)
     no_of_stocks = len(purchase_stock)
-    stock_proportion = proportion[no_of_stocks]
-   
+    
+    if no_of_stocks == 0:
+        return
+    
     money_left = money
     for i in xrange(0, no_of_stocks):
         stock_name = purchase_stock[i][0]
@@ -143,10 +186,13 @@ def process(m, k, d, name, owned, prices):
     for i in xrange(0, k):
         stock_name = name[i]
         stock_owned = owned[i]
-	stock_prices = prices[i]
-	#print "Stock name : {0} , Stock owned : {1}, Stock prices : {2}".format(stock_name, stock_owned, stock_prices)
-        stocks[stock_name] = stock_prices
-    analyse(m, name, owned) 
+        stock_prices = prices[i]
+        if stock_name in stocks and len(stocks[stock_name]) > 0:
+            stocks[stock_name].append(stock_prices[-1])
+        else:
+            stocks[stock_name] = stock_prices
+        #print "Stock name : {0} , Stock owned : {1}, Stock prices : {2}".format(stock_name, stock_owned, stock_prices)
+    analyse(m, name, owned, d) 
     writeData()
 
 # Tail starts here
@@ -166,3 +212,4 @@ if __name__ == '__main__':
     process(m, k, d, names, owned, prices)
 
     printTransaction()
+
